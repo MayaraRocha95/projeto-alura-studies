0aac3b4c8f9aac11b01c75789ec4f22e5c7025bd
Mayara Rocha <mayaraaraujorocha54@gmail.com>
2022-08-30 00:33:37 -0300

    adiconando projeto

---
+++ src/components/Lista/Item/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { ITarefa } from '../../../types/tarefas';
+import style from  './item.module.scss';
+
+interface Props extends ITarefa{
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+
+export default function Item(
+  { tarefa,
+    tempo,
+    selecionado,
+    completado,
+    id,
+   selecionaTarefa }:
+    Props) 
+ {
+console.log('item atual: ', { tarefa, tempo, selecionado, completado, id });
+  return (
+    <li className={`${style.item} ${selecionado ? style.itemSelecionado : ''} 
+      ${completado ? style.itemCompletado : ''}`}
+    onClick={() => !completado && selecionaTarefa(
+      {
+          tarefa,
+          tempo,
+          selecionado,
+          completado,
+          id
+      } 
+      )}>
+      <h3>{tarefa}</h3>
+      <span>{tempo}</span>
+      {completado && <span className={style.concluido}
+      arial-label="Tarefa concluida"
+      ></span>}
+    </li>
+  )
+}
